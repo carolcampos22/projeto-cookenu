3f355351b356ee0b656421491cd5e890fc6fabbf
Carolina <carolina@Carolina.Carolina>
2023-04-28 22:07:15 -0300

    página de login concluída

--- cookenu/src/pages/login/index.js
+++ cookenu/src/pages/login/index.js
@@ -1,5 +1,81 @@
+import { useForm } from "../../hooks"
+import { useState } from 'react'
+import {
+    LoginPageContainer,
+    FormContainer
+} from './styled'
+import {
+    FormControl,
+    FormLabel,
+    Input,
+    FormErrorMessage,
+    InputRightElement,
+    InputGroup,
+    FormHelperText,
+    Button
+} from '@chakra-ui/react'
+
 export const LoginPage = () => {
+
+    const { form, onChangeInputs, clearInputs } = useForm({
+        name: "",
+        password: ""
+    })
+
+    const [isEmailValid, setIsEmailValid] = useState(true)
+    const [showPassword, setShowPassword] = useState(false)
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+        console.log(form)
+        setIsEmailValid(/[a-zA-Z0-9]+@[a-z]{3}[.a-z]?/.test(form.email))
+
+    }
+
+    const onClickShowPassword = () => {
+        setShowPassword(!showPassword)
+    }
+
     return (
-        <h1>Login</h1>
+        <LoginPageContainer>
+            <FormContainer>
+                <form onSubmit={onSubmit}>
+                    <FormControl isInvalid={!isEmailValid}>
+                        <FormLabel>Email</FormLabel>
+                        <Input
+                            name="email"
+                            //  type='email'
+                            value={form.email}
+                            onChange={onChangeInputs}
+                            placeholder="E-mail"
+                        />
+                        {!isEmailValid ? (
+                            <FormErrorMessage as="p">
+                                E-mail inválido.
+                            </FormErrorMessage>
+                        ) : undefined}
+                    </FormControl>
+                    <FormControl>
+                    <FormLabel>Senha</FormLabel>
+                        <InputGroup size='md'>
+                            <Input
+                                name="password"
+                                value={form.password}
+                                onChange={onChangeInputs}
+                                pr='4.5rem'
+                                type={showPassword ? 'text' : 'password'}
+                                placeholder='Senha'
+                            />
+                            <InputRightElement width='4.5rem'>
+                                <Button h='1.75rem' size='sm' onClick={onClickShowPassword}>
+                                    {showPassword ? 'Hide' : 'Show'}
+                                </Button>
+                            </InputRightElement>
+                        </InputGroup>
+                    </FormControl>
+                    <Button type="submit" variant="form">Enviar</Button>
+                </form>
+            </FormContainer>
+        </LoginPageContainer>
     )
 }
